6401e022cdae016bce3f1de3a752013266545c94
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-18 15:34:02 -0500

    Add logic for rock paper scissors

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,42 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK,
+    PAPER,
+    SCISSOR;
+
+
+    private boolean forWinner = false;
 
     public RockPaperScissorHandSign getWinner() {
-        return null;
+        forWinner = true;
+        return getRockPaperScissorHandSign();
+
     }
 
     public RockPaperScissorHandSign getLoser() {
-        return null;
+        forWinner = false;
+        return getRockPaperScissorHandSign();
+    }
+
+    private RockPaperScissorHandSign getRockPaperScissorHandSign() {
+        RockPaperScissorHandSign winner = null;
+        RockPaperScissorHandSign looser = null;
+
+
+        RockPaperScissorHandSign rock = RockPaperScissorHandSign.ROCK;
+        RockPaperScissorHandSign paper = RockPaperScissorHandSign.PAPER;
+        RockPaperScissorHandSign scissors = RockPaperScissorHandSign.SCISSOR;
+
+        if (this == rock) winner =  PAPER;
+        if (this == paper) winner =  SCISSOR;
+        if (this == scissors) winner =  ROCK;
+
+        if (this == rock) looser =  SCISSOR;
+        if (this == paper) looser =  ROCK;
+        if (this == scissors) looser =  PAPER;
+
+        if (this.forWinner) return winner;
+        return looser;
     }
 }
