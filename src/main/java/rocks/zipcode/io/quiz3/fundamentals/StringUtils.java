b499d0e4e4d188f4f313190470e21ea89f577844
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-18 14:27:55 -0500

    Add logic for character at index

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -8,6 +8,9 @@ import java.util.List;
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
+
+    static int counter = 0;
+
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
         char character;
         String result = "";
@@ -18,10 +21,18 @@ public class StringUtils {
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+
+        for (int i = 0; i < baseString.length(); i++){
+            if (baseString.charAt(indexOfString) == characterToCheckFor){
+                return true;
+            }
+        }
+
+            return false;
     }
 
     public static String[] getAllSubStrings(String string) {
+
         ArrayList<String> temp = new ArrayList<>();
         String [] word = string.split("");
         String interim = "";
@@ -31,9 +42,11 @@ public class StringUtils {
             interim = "";
             temp.add(word[i]);
             interim = word[i];
+            counter++;
             for (int k = i + 1; k < word.length; k ++){
                 interim = interim.concat(word[k]);
                 temp.add(interim);
+                counter++;
             }
         }
         result = new String[temp.size()];
@@ -41,6 +54,12 @@ public class StringUtils {
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        String [] temp = input.split(" ");
+
+        for (String each : temp){
+            getAllSubStrings(each);
+        }
+
+        return counter;
     }
 }
