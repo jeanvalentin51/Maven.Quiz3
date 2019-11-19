f5164ec15f4a8616ee1a4c8a02fb3d00ff5a6572
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-18 19:51:29 -0500

    Add logic for wave

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -3,16 +3,29 @@ package rocks.zipcode.io.quiz3.arrays;
 
 import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        String [] result = str.split("");
+
+        ArrayList<String> temp = new ArrayList<>();
+        str = str.toLowerCase();
 
         for (int i = 0; i < str.length(); i++) {
-            StringUtils.capitalizeNthCharacter(str, i);
+            if (Character.isLetter(str.charAt(i))) {
+                temp.add(StringUtils.capitalizeNthCharacter(str, i));
+            } else {
+
+            }
         }
+
+        String[] result = new String[temp.size()];
+        temp.toArray(result);
+
         return result;
     }
 }
